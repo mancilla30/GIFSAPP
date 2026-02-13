7f2535f82f16ff0e4c8bac60103dedef57e43e9c
Mancilla <lm15029@ues.edu.sv>
2026-02-13 12:17:43 +1000

    add Giphy response interfaces

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -2,19 +2,43 @@ import {CustomHeader} from "./shared/components/CustomHeader.tsx";
 import {SearchBar} from "./shared/components/SearchBar.tsx";
 import {PreviousSearches} from "./gifs/components/PreviousSearches.tsx";
 import {GifList} from "./gifs/components/GifList.tsx";
-import {mockGifs} from "./mock-data/gifs.mock.tsx";
+
+import {useState} from "react";
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.actions.ts";
+import type { Gif } from "./gifs/interfaces/gif.interface.ts";
 
 export const GifsApp=()=>{
+    const [gifs, setGifs] = useState<Gif[]>([]);
+    const [previousTerms,setPreviousTerms]=useState<string[]>([]);
+
+    const handleTermClicked=(term:string)=>{
+        console.log({term});
+    }
+    const handleSearch= async(query:string='')=>{
+        query=query.trim().toLowerCase();
+        if (query.length===0)return;
+        if (previousTerms.includes(query))return;
+        setPreviousTerms([query,...previousTerms].splice(0,8));
+        const gifs=await getGifsByQuery(query);
+        setGifs(gifs);
+        
+    }
     return(
         <>
             {/*Header*/}
             <CustomHeader title="Buscador de Gifs" description="descubre y comparte tus gifs favoritos"/>
             {/*Search*/}
-           <SearchBar placeholder="busca lo que quieras"/>
+           <SearchBar
+               placeholder="busca lo que quieras"
+               onQuery={handleSearch}/>
+
             {/*Busquedas Previas*/}
-           <PreviousSearches searches={['Goku','Dragon Ball Z']}/>
+           <PreviousSearches
+               searches={previousTerms}
+               onLabelClicked={handleTermClicked}/>
+
             {/*Gifs*/}
-            <GifList gifs={mockGifs}/>
+            <GifList gifs={gifs}/>
         </>
     )
 }
--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,12 +1,42 @@
+import {type KeyboardEventHandler, useEffect, useState} from "react";
+
 interface Props{
     placeholder?:string;
+    onQuery:(query:string)=>void;
 }
 
-export const SearchBar=({placeholder='Buscar'}:Props)=>{
+export const SearchBar=({placeholder='Buscar',onQuery}:Props)=>{
+    const [query,setQuery]=useState('');
+
+    useEffect(() => {
+      const timeoutId = setTimeout(()=>{
+          onQuery(query);
+      },700);
+      return()=>{
+          clearTimeout(timeoutId);
+      }
+    },[query,onQuery]);
+
+    const handleSearch=()=>{
+        onQuery(query);
+        //setQuery('');
+    }
+
+    const handleKeyDown=(e: KeyboardEventHandler<HTMLInputElement> )=>{
+        if(e.key==='Enter'){
+            handleSearch();
+        }
+    };
     return(
         <div className="search-container">
-            <input type="text" placeholder={placeholder}/>
-            <button>Buscar</button>
+            <input
+                type="text"
+                placeholder={placeholder}
+                value={query}
+                onChange={(e)=>setQuery(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
+            <button onClick={handleSearch}>Buscar</button>
         </div>
-    )
-}
+    );
+};
